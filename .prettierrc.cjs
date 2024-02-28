28e4062a22b38930f4efd86d6582df8a698f2aa6
Serafin Quesada <quesada.serafin03@gmail.com>
2024-02-28 12:54:08 -0300

    tailwind plugin working

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	singleQuote: true,
+	printWidth: 80,
+	tabWidth: 4,
+	requireConfig: false,
+	useTabs: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	semi: false,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
+
+
