81df94616046fde788e937e3ef3a4c657f1ebc2a
Serafin Quesada <quesada.serafin03@gmail.com>
2024-03-11 22:26:06 -0300

    changing in texts and fonts

--- src/components/subtitle.tsx
+++ src/components/subtitle.tsx
@@ -1,6 +1,6 @@
 const Subtitle = () => {
 	return (
-		<h3 className="font-light py-4 pl-1 text-2xl text-[#f9f9f9]">
+		<h3 className="py-4 pl-1 text-2xl font-light text-[#f9f9f9]">
 			Argentinian Front-End Developer +0.5 YOE
 		</h3>
 	)
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -4,48 +4,48 @@ const Title = () => {
 	return (
 		<div className="text-[4rem]   font-semibold  text-[#f9f9f9] sm:text-8xl md:text-[10rem] xl:text-[12rem]">
 			<div>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-300">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-300">
 					S
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-400">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-400">
 					e
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-500">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-500">
 					r
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-600">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-600">
 					a
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-700">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-700">
 					f
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-900">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-900">
 					i
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-950">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-950">
 					n
 				</span>
 			</div>
 			<div>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-950">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-950">
 					Q
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-900">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-900">
 					u
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-800">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-800">
 					e
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-700">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-700">
 					s
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-600">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-600">
 					a
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-400">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-400">
 					d
 				</span>
-				<span className="transition-colors duration-1000 hover:pr-1  hover:text-cyan-300">
+				<span className="duration-900 transition-colors hover:pr-1  hover:text-cyan-300">
 					a
 				</span>
 			</div>
