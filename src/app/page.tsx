9c6281b7913e1a599286d2b89481bc2d202007ce
Serafin Quesada <quesada.serafin03@gmail.com>
2024-03-11 22:24:31 -0300

    improving styles main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@ import About from '@/components/about'
 import Navbar from '@/components/navbar'
 import Subtitle from '@/components/subtitle'
 import Title from '@/components/title'
+import Works from '@/components/works'
 
 export default function Home() {
 	return (
-		<main className="  mx-10 flex h-screen min-h-screen flex-col lg:mx-20 ">
+		<main className="  mx-10 flex flex-col lg:mx-20 ">
 			<div className="flex flex-col">
-				<div className=" flex h-screen flex-col justify-between">
-					<div className='pt-[12rem]'>
+				<div className=" flex h-[60vh] flex-col justify-center md:justify-between">
+					<div className="md:pt-[12rem]">
 						<Title />
 						<Subtitle />
 					</div>
-					<Navbar />
 				</div>
+				<Navbar />
 				<About />
+				<Works />
 			</div>
 		</main>
 	)
