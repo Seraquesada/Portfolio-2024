b065e359a1d1aca3e2434012038625986c5821d9
Serafin Quesada <quesada.serafin03@gmail.com>
2024-03-23 15:08:10 -0300

    finish project card

--- src/components/cards/projectsCard.tsx
+++ src/components/cards/projectsCard.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import Link from 'next/link'
+import { FC } from 'react'
 
-const ProjectsCard = () => {
-    return <div>ProjectsCard</div>;
-};
+interface Props {
+	title: string
+	date: string
+	description: string
+	stack: string
+	webSite: string
+	github: string
+}
 
-export default ProjectsCard;
+const ProjectsCard: FC<Props> = ({
+	title,
+	date,
+	description,
+	stack,
+	webSite,
+	github,
+}) => {
+	return (
+		<div className="flex w-full flex-col gap-2  md:w-3/5 ">
+			<div className="flex flex-col items-end justify-start gap-3 sm:flex-row">
+				<h4 className="text-3xl font-semibold ">{title}</h4>
+				<span className="text-xl font-light  md:pl-6">{date}</span>
+			</div>
+			<p className="w-4/5  text-lg">{description}</p>
+			<p>
+				<span className="font-semibold">Tech Stack: </span>
+				{stack}
+			</p>
+			<div className="flex gap-5">
+				<p className="font-semibold uppercase hover:text-cyan-500">
+					web site
+				</p>
+				<Link
+					className="font-semibold uppercase hover:text-cyan-500"
+					href={github}
+					target="_blank"
+				>
+					Git Hub
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectsCard
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,37 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { FC } from 'react'
+import ProjectsCard from './cards/projectsCard'
 
-const Projects = () => {
+interface Projects {
+	title: string
+	date: string
+	description: string
+	stack: string
+	webSite: string
+	github: string
+}
+
+const projects: Projects[] = [
+	{
+		title: 'Front-End - Digital Booking',
+		date: '19/10/2022 - 12/12/2022',
+		description:
+			'A car rental system where you can choose base on location and category the car you want.',
+		stack: 'HTML, CSS, JavaScript, React Js, Next Js, Java, SpringBoot, Postman, AWS, Git and Github',
+		webSite: '',
+		github: 'https://github.com/Seraquesada/ProyectoFinal-DH/',
+	},
+	{
+		title: 'Front-End - PassPortal',
+		date: '15/10/2023 - 15/12/2023',
+		description:
+			'A web where you can find the lastest events and get your tickets to attend to them.',
+		stack: 'HTML, CSS, JavaScript, React Js, Next Js, Java, SpringBoot, Postman, AWS, Git and Github',
+		webSite: '',
+		github: 'https://github.com/Seraquesada/PassPortal/',
+	},
+]
+
+const Projects: FC = () => {
 	return (
 		<section
 			id="projects"
@@ -11,78 +40,17 @@ const Projects = () => {
 			<div className="flex flex-col gap-5">
 				<h2 className="text-6xl"> Projects </h2>
 				<div className="flex w-full flex-col gap-5 lg:flex-row ">
-					<div className="flex w-full flex-col gap-2  md:w-3/5 ">
-						<div className="flex flex-col items-end justify-start gap-3 sm:flex-row">
-							<h4 className="text-3xl font-semibold ">
-								Front-End - Digital Booking
-							</h4>
-							<span className="text-xl font-light  md:pl-6">
-								19/10/2022 - 12/12/2022
-							</span>
-						</div>
-
-						<p className="w-4/5  text-lg">
-							A car rental system where you can choose base on
-							location and category the car you want.
-						</p>
-						<p>
-							<span className="font-semibold">Tech Stack: </span>
-							HTML, CSS, JavaScript, React Js, Java, SpringBoot,
-							Postman, AWS, Git and Gitlab
-						</p>
-						<div className="flex gap-5">
-							<p
-								className="font-semibold uppercase hover:text-cyan-500"
-								// href="https://upler.tech/"
-								// target="_blank"
-							>
-								web site
-							</p>
-							<Link
-								className="font-semibold uppercase hover:text-cyan-500"
-								href="https://github.com/Seraquesada/PassPortal/"
-								target="_blank"
-							>
-								Git Hub
-							</Link>
-						</div>
-					</div>
-					<div className="flex w-full flex-col gap-2  md:w-3/5 ">
-						<div className="flex flex-col items-end justify-start gap-3 sm:flex-row">
-							<h4 className="text-3xl font-semibold ">
-								Front-End - PassPortal
-							</h4>
-							<span className="text-xl font-light  md:pl-6">
-								15/10/2023 - 15/12/2023
-							</span>
-						</div>
-
-						<p className="w-4/5  text-lg">
-							A web where you can find the lastest events and get
-							your tickets to attend to them.
-						</p>
-						<p>
-							<span className="font-semibold">Tech Stack: </span>
-							HTML, CSS, JavaScript, React Js, Next Js, Java,
-							SpringBoot, Postman, AWS, Git and Github
-						</p>
-						<div className="flex gap-5">
-							<p
-								className="font-semibold uppercase hover:text-cyan-500"
-								// href="https://upler.tech/"
-								// target="_blank"
-							>
-								web site
-							</p>
-							<Link
-								className="font-semibold uppercase hover:text-cyan-500"
-								href="https://github.com/Seraquesada/PassPortal"
-								target="_blank"
-							>
-								Git Hub
-							</Link>
-						</div>
-					</div>
+					{projects.map((project) => (
+						<ProjectsCard
+							key={project.title}
+							title={project.title}
+							date={project.date}
+							description={project.description}
+							stack={project.stack}
+							webSite={project.webSite}
+							github={project.github}
+						/>
+					))}
 
 					{/* <div className="flex w-full flex-col gap-2  md:w-3/5 ">
 						<div className="flex flex-col items-end justify-start gap-3 sm:flex-row">
