9accc62dfe85eed70443ca27abb420c5ceb9201d
Serafin Quesada <quesada.serafin03@gmail.com>
2024-03-26 19:03:46 -0300

    adding dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Add a rule to handle PDF files
+        config.module.rules.push({
+            test: /\.(pdf)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next/static/files',
+                        outputPath: `${isServer ? '../' : ''}static/files`,
+                        name: '[name].[ext]',
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
